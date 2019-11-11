ef683569238e20a003088cee43f4f50aa476ce3b
saarshoer <50770012+saarshoer@users.noreply.github.com>
2019-11-11 13:27:16 +0200

    Add files via upload

---
+++ mmmbp_run.py
@@ -0,0 +1,28 @@
+import os
+from LabMBPipeline.mmmbp import get_args, main
+
+input_dir = r'/net/mraid08/export/genie/Data/ExternalSamples/IBD/metagenomics'
+output_dir = r'/net/mraid08/export/jafar/Microbiome/Analyses/saar/IBD/mmmbp'
+
+module_seq = 'RAW,QCS,HGF,UNT,URA'
+
+df_path = os.path.join(output_dir, 'DFOut')
+tmp_dir = os.path.join(output_dir, 'tmp')
+email = 'saar.shoer@weizmann.ac.il'
+max_r = '550'
+
+args, sm = get_args([
+
+                     df_path,
+                     tmp_dir,
+                     email,
+
+                     '--module_seq', module_seq,
+                     '--use_general_python',
+                     '--max_r', max_r,
+
+                     '--raw_input_folders', input_dir
+
+                     ])
+
+main(args, sm)
