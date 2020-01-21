f5eca6cf104aa400f36fc251674f72ed44e86163
saarsh <iaxq2kkh>
2020-01-21 17:33:56 +0200

    MSE note

--- pipeline.py
+++ pipeline.py
@@ -4,7 +4,7 @@ from LabMBPipeline.mmmbp import get_args, main
 input_dir = r'/net/mraid08/export/genie/Data/ExternalSamples/IBD/metagenomics'
 output_dir = r'/net/mraid08/export/jafar/Microbiome/Analyses/saar/IBD/mmmbp'
 
-module_seq = 'RAW,QCS,HGF,UNT,URA'
+module_seq = 'RAW,QCS,HGF,UNT,URA'  # maybe should have had MSE
 
 df_path = os.path.join(output_dir, 'DFOut')
 tmp_dir = os.path.join(output_dir, 'tmp')
