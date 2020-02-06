4c43a9f3ddfee591ee26151b960656944f42fa57
saarsh <iaxq2kkh>
2020-02-06 17:28:03 +0200

    .

--- dissimilarity.py
+++ dissimilarity.py
@@ -2,29 +2,32 @@ import os
 from LabData import config_global as config
 from LabUtils.addloglevels import sethandlers
 from LabUtils.Utils import date2_dir, write_members, to_list
+from LabData.DataMergers.MultiDataLoader import MultiDataLoader
+from LabData.DataAnalyses.MBSNPs.MBSNPAnalyses import MBSNPPairwiseDistances
 
 
-class p():
+class P:
+    species_set = ['SGB_2304']#None
+    send_to_queue = False
+    largest_sample_per_user = False
 
-    min_positions_per_sample = 20 if config.DEBUG else 20000
-    min_common_positions = 20000
-    send_to_queue = False if config.DEBUG else True
+    body_site = 'Gut'
+    min_subjects_per_snp = 400
+    min_reads_per_snp = 3
 
-    species_list = None
-
-    species_set = ['SGB_1814'] if config.DEBUG else species_list
-
-    study_ids = ['PNP1']
+    # body_site = 'Oral'
+    # min_subjects_per_snp = 10
+    # min_reads_per_snp = 1
 
     subjects_loaders = ['SubjectLoader']
+
+    study_ids = ['PNP3']
     subjects_get_data_args = {
         'study_ids': study_ids,
-        'groupby_reg': 'first',
     }
 
 
 def data_gen(loaders, subjects_df=None, **kwargs):
-    from LabData.DataMergers.MultiDataLoader import MultiDataLoader
     accepts_subjects_df = all([l != 'SubjectLoader' for l in to_list(loaders)])
     return MultiDataLoader(loaders, subjects_df=subjects_df, **kwargs).get_data() if accepts_subjects_df \
         else MultiDataLoader(loaders, **kwargs).get_data()
@@ -32,17 +35,18 @@ def data_gen(loaders, subjects_df=None, **kwargs):
 
 def gen_pairwise_dists():
     work_dir = os.path.join(config.analyses_dir, date2_dir())
-    write_members(os.path.join(work_dir, 'PARAMS.txt'), p)
+    write_members(os.path.join(work_dir, 'PARAMS.txt'), P)
 
-    subjects_gen_f = lambda: data_gen(p.subjects_loaders, **p.subjects_get_data_args)
+    subjects_gen_f = lambda: data_gen(P.subjects_loaders, **P.subjects_get_data_args)
 
-    from LabData.DataAnalyses.MBSNPs.MBSNPAnalyses import MBSNPPairwiseDistances
+    MBSNPPairwiseDistances(work_dir=work_dir,
+                           send_to_queue=P.send_to_queue,
 
-    MBSNPPairwiseDistances(min_common_positions=p.min_common_positions, send_to_queue=p.send_to_queue,
-                           min_positions_per_sample=p.min_positions_per_sample,
-                           work_dir=work_dir,
-                           largest_sample_per_user=False).\
-        run(subjects_gen_f=subjects_gen_f, species_set=p.species_set)
+                           body_site=P.body_site,
+                           min_reads_per_snp=P.min_reads_per_snp,
+                           min_subjects_per_snp=P.min_subjects_per_snp,
+                           largest_sample_per_user=P.largest_sample_per_user). \
+        run(subjects_gen_f=subjects_gen_f, species_set=P.species_set)
 
 
 if __name__ == '__main__':
