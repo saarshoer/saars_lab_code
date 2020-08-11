774a28307b608ca9b2774e1e7d9d06162ab17b92
saarsh <iaxq2kkh>
2020-08-11 10:48:09 +0300

    conclusion from failed run

--- antibiotics_mwas.py
+++ antibiotics_mwas.py
@@ -14,15 +14,16 @@ class P:
     countries = ['IL']
 
     # queue
-    max_jobs = 300
-    jobname = 'anti'
+    max_jobs = 50
+    jobname = 'anti_mwas'
     send_to_queue = True#False
     work_dir = os.path.join(config.analyses_dir, date2_dir())
+    work_dir_suffix = jobname
 
     # species
-    species_set = None#['SGB_2318', 'SGB_1815']
+    species_set = 'SGB_4866'#None#['SGB_2318', 'SGB_1815']
     ignore_species = None
-    species_blocks = 5
+    species_blocks = 1
 
     # samples
     largest_sample_per_user = True
@@ -32,7 +33,7 @@ class P:
     min_reads_per_snp = 1  # in maf file name
     min_subjects_per_snp_cached = 500  # in maf file name
     max_on_fraq_major_per_snp = 0.98  # (Liron 0.99) Max fraction of major allele frequency in analyzed samples
-    min_on_minor_per_snp = 100  # (Liron 50)Min number of analyzed samples with a minor allele
+    min_on_minor_per_snp = 100  # (Liron 50) Min number of analyzed samples with a minor allele
     min_subjects_per_snp = 1000  # (Liron 400)
     snp_set = None
 
@@ -48,24 +49,19 @@ class P:
     # y
     y_loaders = [body_site + 'MBLoader']
     y_get_data_args = {**{'df': 'segata_species', 'study_ids': study_ids, 'genotek_vals': None, 'min_hgf_fraction': None,
-                       'min_reads': None, 'nextera': None, 'reg_ids': None, #'subjects_df': None,
-                       'largest_sample_per_user': largest_sample_per_user, 'min_col_val': None, 'min_col_present': None,
-                       'min_col_present_frac': None, 'top_frac_present_col': None, 'take_log': False,
-                       'convert_to_binary': False, 'groupby_reg': None}}
-                       # eliminate in full run
-                       # 'cols': ['k__Bacteria|p__Bacteroidetes|c__Bacteroidia|o__Bacteroidales|f__Rikenellaceae|g__Alistipes|s__Alistipes_putredinis|fSGB__673|gSGB__1686|sSGB__2318',
-                       #          'k__Bacteria|p__Bacteroidetes|c__Bacteroidia|o__Bacteroidales|f__Bacteroidaceae|g__Bacteroides|s__Bacteroides_dorei|fSGB__595|gSGB__1353|sSGB__1815']}
-    #min_reads
-    take_log = True
-    min_col_val = -4  # in log space
-    # min_col_present/_frac = 0.05 # Select columns that are present in at least min_col_present/_frac of the samples
+                          'min_reads': None, 'nextera': None, 'reg_ids': None, #'subjects_df': None,
+                          'largest_sample_per_user': largest_sample_per_user, 'min_col_val': -4, 'min_col_present': None,
+                          'min_col_present_frac': None, 'top_frac_present_col': None, 'take_log': True,
+                          'convert_to_binary': False, 'groupby_reg': None}}
+                          # eliminate in full run
+                          # 'cols': ['k__Bacteria|p__Bacteroidetes|c__Bacteroidia|o__Bacteroidales|f__Rikenellaceae|g__Alistipes|s__Alistipes_putredinis|fSGB__673|gSGB__1686|sSGB__2318',
+                          #          'k__Bacteria|p__Bacteroidetes|c__Bacteroidia|o__Bacteroidales|f__Bacteroidaceae|g__Bacteroides|s__Bacteroides_dorei|fSGB__595|gSGB__1353|sSGB__1815']}
     is_y_valid_f = None  # Function that checks whether the analyzed y is valid
 
     output_cols = None
 
 
 if __name__ == '__main__':
-    write_members(os.path.join(P.work_dir, 'PARAMS.txt'), P)
     sethandlers(file_dir=config.log_dir)
     m = MWAS(P)
     work_dir = m.gen_mwas()
