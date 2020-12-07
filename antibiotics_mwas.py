1540c3c58c82aa1cf0cf56ecbef0049f2f4e71bd
saarsh <iaxq2kkh>
2020-12-07 19:59:48 +0200

    main annotation files

--- antibiotics_mwas.py
+++ antibiotics_mwas.py
@@ -2,8 +2,10 @@ import os
 # os.environ['OMP_NUM_THREADS'] = '2'  # Keep OMP from taking many CPUs.
 
 import glob
+import numpy as np
 import pandas as pd
-from LabQueue.qp import qp
+from LabQueue.qp import qp, fakeqp
+from matplotlib.lines import Line2D
 from LabData import config_global as config
 from LabUtils.addloglevels import sethandlers
 from LabData.DataLoaders.Loader import LoaderData
@@ -72,6 +74,57 @@ def y_gen_f_inner(subjects_df, y):
     return LoaderData(pd.DataFrame(y), None)
 
 
+def text_func_annotated_between(df: pd.DataFrame):
+    text = f"{df.shape[0]} SNPs in {df.index.get_level_values('Species').unique().shape[0]} species\n" + \
+           f"N = between {df['N'].min()} and {df['N'].max()} samples per SNP\n" + \
+           f"{sum(df['Global_Bonferroni'] <= 0.05)} SNPs passed 0.05 Global Bonferroni cutoff"
+    return text
+
+
+def text_func_annotated_within(df: pd.DataFrame):
+    text = f"{sum(df['Global_Bonferroni'] <= 0.05)}/{df.shape[0]} SNPs passed 0.05 Global Bonferroni cutoff\n" + \
+           f"N = between {df['N'].min()} and {df['N'].max()} samples per SNP"
+    return text
+
+
+def draw_func_annotated(sp_df: pd.DataFrame, **kwargs):
+    d = dict()
+
+    black = (0.0, 0.0, 0.0, 0.7)
+    white = (1.0, 1.0, 1.0, 0.7)
+    blue = (0.0, 0.0, 1.0, 0.7)
+    red = (1.0, 0.0, 0.0, 0.7)
+
+    marker_dict = {'intergenic': '*', 'synonymous': 'o', 'non-synonymous': 'o', 'non-protein': 'v'}
+    facecolor_marker_dict = {'intergenic': black, 'synonymous': white, 'non-synonymous': black, 'non-protein': black}
+    facecolor_dict = {True: red, False: blue}
+    facecolor_label_dict = {True: 'positive relationship', False: 'negative relationship'}
+    label_dict = {'intergenic': 'Intergenic (no annotated function)',
+                  'synonymous': 'Protein coding - synonymous mutation',
+                  'non-synonymous': 'Protein coding - non-synonymous mutation',
+                  'non-protein': 'Non-protein coding (rRNA, tRNA etc.)'}
+
+    d['marker'] = sp_df['annotation'].map(marker_dict).values
+    d['s'] = -np.log10(sp_df[kwargs['pval_col']]).values
+    # d['facecolor'] = sp_df['annotation'].map(facecolor_dict).values
+    d['facecolor'] = (sp_df['Coef'] > 0).map(facecolor_dict).values
+    d['edgecolor'] = black
+    d['linewidths'] = 1
+    d['alpha'] = 0.5
+
+    legend_mutation = [Line2D([0], [0], linewidth=0, label=label_dict[k], alpha=d['alpha'],
+                     markersize=10, marker=marker_dict[k], markeredgewidth=d['linewidths'],
+                     markerfacecolor=facecolor_marker_dict[k], markeredgecolor=d['edgecolor']) for k in marker_dict.keys()]
+
+    legend_coefficient = [Line2D([0], [0], linewidth=0, label=facecolor_label_dict[k], alpha=d['alpha'],
+                     markersize=10, marker='s', markeredgewidth=0,
+                     markerfacecolor=facecolor_dict[k], markeredgecolor=white) for k in facecolor_dict.keys()]
+
+    legend = legend_mutation + legend_coefficient
+
+    return sp_df, d, legend
+
+
 if __name__ == '__main__':
     # # creating the raw files
     # sethandlers(file_dir=config.log_dir)
@@ -79,44 +132,66 @@ if __name__ == '__main__':
     # work_dir = m.gen_mwas()
 
     # running the interpreter
-    # TODO: perhaps change directories structure so to be one folder per species with within and between inside it
     run_type = 'between_species'
 
     input_path = os.path.join('/net/mraid08/export/genie/LabData/Analyses/saarsh/anti_mwas_processed', run_type)
     output_path = os.path.join('/net/mraid08/export/jafar/Microbiome/Analyses/saar/antibiotics/figs', run_type)
     jobs_path = '/net/mraid08/export/jafar/Microbiome/Analyses/saar/antibiotics/jobs/'
 
+    # annotations
+    annotations_df = pd.read_hdf('/net/mraid08/export/genie/LabData/Analyses/saarsh/anti_mwas_processed/snps_codon.h5')
+    # annotation handling is done with contig without part while mwas data frame is contig with part
+    annotations_df = annotations_df.rename(columns={'Contig_with_part': 'Contig'})
+    annotations_df = annotations_df.reset_index('Contig', drop=True).set_index('Contig', append=True).\
+        reorder_levels(['Species', 'Contig', 'Position'])
+    # annotations that can be deduced (so not saved in the hdf files)
+    annotations_df['NonSymMutation'] = annotations_df['MajorAA'] != annotations_df['MinorAA']
+    annotations_df['annotation'] = annotations_df['NonSymMutation'].map({False: 'synonymous', True: 'non-synonymous'})
+    annotations_df.loc[annotations_df['GeneDistance'] < 0, 'annotation'] = 'intergenic'
+    annotations_df.loc[(annotations_df['GeneDistance'] >= 0) & (annotations_df['feature'] != 'CDS'), 'annotation'] = \
+        'non-protein'  # (df['feature'] != 'CDS') does not cover all options
+    # the only thing needed moving forward
+    annotations_df = annotations_df['annotation']
+
     def do(file_path):
 
-        M = MWASInterpreter(params=P, mwas_fname=os.path.basename(file_path), work_dir=input_path, out_dir=output_path,
-                            mbsnp_loader=get_mbsnp_loader_class(P.body_site),
+        M = MWASInterpreter(params=P, mbsnp_loader=get_mbsnp_loader_class(P.body_site),
+                            mwas_fname=os.path.basename(file_path), work_dir=input_path,
+                            out_dir=output_path, save_by_y=True,
                             pval_col='Global_Bonferroni', pval_cutoff=0.05,
-                            SNPs_to_plot_dct={},
 
-                            do_manhattan_plot=False,  # unnecessary given the annotated manhattan
-                            # TODO: check if in between_species it results in seperate plots per x species
-                            do_mafs_plot=False,  # broken
-                            do_qq_plot=True,
+                            do_manhattan_plot=True,
                             do_volcano_plot=True,
+                            do_qq_plot=True,
+                            # do_mafs_plot=False,  # broken
 
-                            do_snp_annotations=True,
-                            annotate_all_snps=True,
-                            do_annotated_manhattan=True,
-                            # TODO: within plot does not look good - need to debug
+                            # do_snp_annotations=True,
+                            # annotate_all_snps=True,
+                            # do_annotated_manhattan=True,  # unnecessary given the modular manhattan
 
-                            get_extra_gene_info=True,
-                            do_find_surrounding_genes=True,
-                            do_test_nonsynonymous_enrichment=True,
-                            ).run()
+                            # get_extra_gene_info=True,
+                            # do_find_surrounding_genes=True,
+                            # do_test_nonsynonymous_enrichment=True,
+                            )
+
+        M._mwas_df = pd.read_hdf(os.path.join(M._work_dir, M._mwas_fname))
+        M._mwas_df = M._mwas_df.join(annotations_df, on=['Species', 'Contig', 'Position'])
+        M._draw_func = draw_func_annotated
+        M._text_func = text_func_annotated_between if run_type == 'between_species' else text_func_annotated_within
+
+        M.run()
+
+
+    # queue
 
     os.chdir(jobs_path)
     sethandlers(file_dir=jobs_path)
     # TODO: between needs more memory
-    with qp(jobname=run_type, _delete_csh_withnoerr=True, q=['himem7.q'], max_r=50) as q:
+    with fakeqp(jobname=run_type, _delete_csh_withnoerr=True, q=['himem7.q'], max_r=50) as q:
         q.startpermanentrun()
         tkttores = {}
 
-        for file in glob.glob(os.path.join(input_path, 'SGB_9712.h5')):
+        for file in glob.glob(os.path.join(input_path, 'SGB_9712.h5')):#10068
             tkttores[file] = q.method(do, [file])
 
         for k, v in tkttores.items():
--- mwas_annotations.py
+++ mwas_annotations.py
@@ -13,52 +13,42 @@ from UseCases.DataAnalyses.mbsnps_annotation import Annotationist
 from LabData.DataLoaders.MBSNPLoader import get_mbsnp_loader_class
 from LabData.DataLoaders.GeneAnnotLoader import GeneAnnotLoader, ANNOTS_03_2020_EXPANDED
 
-# parameters
-x_mwas_files_path = '/net/mraid08/export/genie/LabData/Analyses/saarsh/anti_mwas_raw/mb_gwas_SGB_*.h5'
-y_mwas_files_path = '/net/mraid08/export/genie/LabData/Analyses/saarsh/anti_mwas_processed/*/SGB_*.h5'
-annotations_files_path = '/net/mraid08/export/genie/LabData/Analyses/saarsh/anti_mwas_processed'
-jobs_path = '/net/mraid08/export/jafar/Microbiome/Analyses/saar/antibiotics/jobs'
-from antibiotics_mwas import P
 
+def find_unique_snps(x_mwas_files_path, y_mwas_files_path, output_path):
+    # find unique snps and count comparisons (for p_value calculations)
 
-def do():
-
-    # phase 1
     x_mwas_files = glob.glob(x_mwas_files_path)
     y_mwas_files = glob.glob(y_mwas_files_path)
 
     x_species = list(set(['SGB' + file.split('SGB')[-1].split('.')[0] for file in x_mwas_files]))
     y_species = list(set([os.path.basename(file).split('.')[0] for file in y_mwas_files]))
 
+    # pre run
+    snps = set()
     pvals_df = pd.DataFrame(index=y_species, columns=x_species)
 
-    snps = set()
-
+    # run
     for x_mwas_file in x_mwas_files:
-        species = 'SGB' + x_mwas_file.split('SGB')[-1].split('.')[0]
+        x_species = 'SGB' + x_mwas_file.split('SGB')[-1].split('.')[0]
         with HDFStore(x_mwas_file, 'r') as x_mwas_df:
-            x_mwas_df = x_mwas_df[species]
+            x_mwas_df = x_mwas_df[x_species]
 
-        values, counts = np.unique(x_mwas_df.index.get_level_values('Y'), return_counts=True)
-        pvals_df.loc[values.tolist(), species] = counts.tolist()
+        y_species, y_species_count = np.unique(x_mwas_df.index.get_level_values('Y'), return_counts=True)
+        pvals_df.loc[y_species.tolist(), x_species] = y_species_count.tolist()
 
         snps = snps.union(set(x_mwas_df.index.droplevel('Y').values))
 
+    # post run
     snps = pd.DataFrame(snps)
+    snps.columns = ['Species', 'Contig', 'Position']
+    snps = snps.set_index(['Species', 'Contig', 'Position'])
+    pvals_df.to_csv(os.path.join(output_path, 'pvals_count.csv'))
+    snps.to_hdf(os.path.join(output_path, 'snps.h5'), key='snps')
 
-    pvals_df.to_csv(os.path.join(annotations_files_path, 'pvals_count.csv'))
+    return snps
 
-    snps.to_hdf(os.path.join(annotations_files_path, 'snps.h5'), key='snps')
 
-    # phase 2
-    def _add_snp_annotations(species, df):
-        maf_annot_fname = get_mbsnp_loader_class(P.body_site).\
-            get_snp_maf_annot_fname(species, P.min_reads_per_snp, P.min_subjects_per_snp_cached)
-        if os.path.exists(maf_annot_fname):
-            with HDFStore(maf_annot_fname, 'r') as maf_annot:
-                maf_annot = maf_annot[species]
-            df = df.join(maf_annot)
-        return df
+def add_contig_without_part(snps, output_path):
 
     def remove_contig_parts(contigs: Iterable[str]) -> List[str]:
         """Returns the given contig names without the trailing '_P###'."""
@@ -74,15 +64,39 @@ def do():
         return [cached_name(x) for x in contigs]
 
     # Remove the part from the contig key
-    snps.columns = ['Species', 'Contig', 'Position']
-    snps['Contig'] = remove_contig_parts(snps['Contig'].values)
-    snps = snps.set_index(snps.columns.tolist())
+    idx = snps.index.names
+    snps = snps.reset_index()
+    snps = snps.rename(columns={'Contig': 'Contig_with_part'})
+    snps['Contig'] = remove_contig_parts(snps['Contig_with_part'].values)
+    snps = snps.set_index(idx)
+
+    snps.to_hdf(os.path.join(output_path, 'snps_contig.h5'), key='snps')
+
+    return snps
+
+
+def add_single_gene_maf_annotations(snps, P, output_path):
+    # add annotations from maf_annot file
+
+    def _add_snp_annotations(species, df):
+        maf_annot_fname = get_mbsnp_loader_class(P.body_site).\
+            get_snp_maf_annot_fname(species, P.min_reads_per_snp, P.min_subjects_per_snp_cached)
+        if os.path.exists(maf_annot_fname):
+            with HDFStore(maf_annot_fname, 'r') as maf_annot:
+                maf_annot = maf_annot[species]
+            df = df.join(maf_annot)
+        return df
 
     snps = pd.concat([_add_snp_annotations(species, df) for species, df in snps.groupby('Species')])
 
-    snps.to_hdf(os.path.join(annotations_files_path, 'snps_maf_annotations.h5'), key='snps')
+    snps.to_hdf(os.path.join(output_path, 'snps_maf_annotations.h5'), key='snps')
 
-    # phase 3
+    return snps
+
+
+def map_codon_information(snps, output_path):
+    # add codon information
+
     # remove the codons extracted from the reference genomes, rather than the MAFs
     snps = snps.drop(columns=['MajorCodon', 'MinorCodon'])
 
@@ -96,26 +110,72 @@ def do():
     snps.loc[is_coding, 'MajorAA'] = snps.loc[is_coding, 'MajorCodonMAF'].apply(translate_codon)
     snps.loc[is_coding, 'MinorAA'] = snps.loc[is_coding, 'MinorCodonMAF'].apply(translate_codon)
 
-    snps.to_hdf(os.path.join(annotations_files_path, 'snps_codon_annotations.h5'), key='snps')
+    snps.to_hdf(os.path.join(output_path, 'snps_codon.h5'), key='snps')
 
-    # phase 4
+    return snps
+
+
+def add_gene_info(snps, output_path, on='GeneID', rsuffix=''):
+    # add gene annotations
+
     genes = GeneAnnotLoader(ANNOTS_03_2020_EXPANDED).get_annot()
-    snps = snps.join(genes.drop(columns=['strand', 'feature']), on='GeneID')
-    # snps = Annotationist(genes, load_sequences=False).lookup_df(snps)  # upstream/downstream, +/- strand genes
+    snps = snps.join(genes.drop(columns=['strand', 'feature']), on=on, rsuffix=rsuffix)
 
-    snps.to_hdf(os.path.join(annotations_files_path, 'snps_gene_annotations.h5'), key='snps')
+    snps.to_csv(os.path.join(output_path, 'snps_gene_annotations.csv'))
 
-    # # phase 5
-    # tax_df = taxonomy_df(level_as_numbers=False).set_index('SGB')[
-    #     ['Kingdom', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species']]
-    # snps = snps.join(tax_df, on='Species')
-    #
-    # snps.to_hdf(os.path.join(annotations_files_path, 'snps_taxonomy_annotations.h5'), key='snps')
+    return snps
 
 
-os.chdir(jobs_path)
-sethandlers()
+def add_surrounding_genes(snps, output_path):
+    # add multiple genes
 
-with qp(jobname='annot', _delete_csh_withnoerr=True, q=['himem7.q'], max_r=1, _mem_def='50G') as q:
-    q.startpermanentrun()
-    q.waitforresult(q.method(do))
+    genes = GeneAnnotLoader(ANNOTS_03_2020_EXPANDED).get_annot()
+    snps = Annotationist(genes, load_sequences=False).lookup_df(snps)  # upstream/downstream, +/- strand genes
+
+    snps.to_hdf(os.path.join(output_path, 'snps_multiple_genes.h5'), key='snps')
+
+    return snps
+
+
+def add_taxonomy(snps, output_path):
+
+    tax_df = taxonomy_df(level_as_numbers=False).set_index('SGB')[
+        ['Kingdom', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species']]
+    snps = snps.join(tax_df, on='Species')
+
+    snps.to_hdf(os.path.join(output_path, 'snps_taxonomy.h5'), key='snps')
+
+    return snps
+
+
+if __name__ == '__main__':
+
+    def antibiotics():
+
+        # parameters
+        x_mwas_files_path = '/net/mraid08/export/genie/LabData/Analyses/saarsh/anti_mwas_raw/mb_gwas_SGB_*.h5'
+        y_mwas_files_path = '/net/mraid08/export/genie/LabData/Analyses/saarsh/anti_mwas_processed/*/SGB_*.h5'
+        output_path = '/net/mraid08/export/genie/LabData/Analyses/saarsh/anti_mwas_processed'
+        from antibiotics_mwas import P
+
+        # run
+        snps = find_unique_snps(x_mwas_files_path, y_mwas_files_path, output_path)
+        snps = add_contig_without_part(snps, output_path)
+        snps = add_single_gene_maf_annotations(snps, P, output_path)
+
+        add_surrounding_genes(snps, output_path)
+
+        snps = map_codon_information(snps, output_path)
+        snps = add_gene_info(snps, output_path)
+
+        return snps
+
+
+    jobs_path = '/net/mraid08/export/jafar/Microbiome/Analyses/saar/antibiotics/jobs'
+
+    os.chdir(jobs_path)
+    sethandlers()
+
+    with qp(jobname='annot', _delete_csh_withnoerr=True, q=['himem7.q'], max_r=1, _mem_def='50G') as q:
+        q.startpermanentrun()
+        q.waitforresult(q.method(antibiotics))
