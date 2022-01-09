49f0d47531bb0274a376343d14fd6f0d7a99449a
saarsh <iaxq2kkh>
2022-01-09 11:23:05 +0200

    update as atopic

--- UseCases/pnp3/pnp3_dissimilarity.py
+++ UseCases/pnp3/pnp3_dissimilarity.py
@@ -1,44 +1,76 @@
 import os
+from LabUtils.Utils import write_members
 from LabData import config_global as config
 from LabUtils.addloglevels import sethandlers
 from LabData.DataLoaders.SubjectLoader import SubjectLoader
-from LabUtils.Utils import date2_dir, write_members, to_list
+from LabData.DataAnalyses.MBSNPs.MBSNPAnalyses import MBSNPPairwiseDistances
 
 
 class P:
+    # data
+    study_ids = ['PNP3']
+    body_site = 'PNP3'
 
-    body_site = 'Gut'
+    # queue
+    max_jobs = 250
     send_to_queue = True
+    jobname = f'diss'
+    verbose = False
+    analyses_dir = config.analyses_dir
+    work_dir_suffix = f'{"_".join(study_ids)}_diss_{body_site.lower()}'
+
+    # species
+    species_set = None
+    ignore_species = None
+    filter_by_species_existence = False
+    species_blocks = 1
+
+    # subjects
+    subjects_gen_f = lambda: SubjectLoader().get_data(study_ids=P.study_ids, groupby_reg='first')
+
+    # samples
+    samples_set = None
     largest_sample_per_user = False
-
-    min_reads_per_snp = 3
-
-    min_common_positions = 20000
     min_positions_per_sample = 20000
+    min_common_positions = 20000
+    subsample_dir = ''
+    other_samples_set = None
 
-    filter_by_species_existence = True
+    # SNPs
+    min_reads_per_snp = 3
+    min_subjects_per_snp = 1
 
-    work_dir = os.path.join(config.analyses_dir, date2_dir())
+    # irrelevant params
+    min_subjects_per_snp_cached = None
+    max_on_fraq_major_per_snp = None
+    min_on_minor_per_snp = None
+    snp_set = None
 
+    # y
+    is_y_valid_f = None
+    max_on_most_freq_val_in_col = None
+    min_on_non_freq_val_for_y = None
 
-def data_gen(loaders, subjects_df=None, **kwargs):
-    from LabData.DataMergers.MultiDataLoader import MultiDataLoader
-    accepts_subjects_df = all([l != 'SubjectLoader' for l in to_list(loaders)])
-    return MultiDataLoader(loaders, subjects_df=subjects_df, **kwargs).get_data() if accepts_subjects_df \
-        else MultiDataLoader(loaders, **kwargs).get_data()
-
-
-def gen_pairwise_dists():
-    write_members(os.path.join(P.work_dir, 'PARAMS.txt'), P)
-
-    def subjects_gen_f():
-        return SubjectLoader().get_data(study_ids=['PNP3'])
-
-    from LabData.DataAnalyses.MBSNPs.MBSNPAnalyses import MBSNPPairwiseDistances
-    MBSNPPairwiseDistances(**dict((key, value) for key, value in P.__dict__.items()
-                                  if not key.startswith('__'))).run(subjects_gen_f=subjects_gen_f)
+    # others
+    mwas_data_clusterer = None
+    groupby_reg = None
 
 
 if __name__ == '__main__':
+
     sethandlers(file_dir=config.log_dir)
-    gen_pairwise_dists()
+
+    m = MBSNPPairwiseDistances(P)
+    write_members(os.path.join(m._work_dir, 'PARAMS.txt'), P)
+    m.run(
+        species_blocks=P.species_blocks,
+        species_set=P.species_set,
+        ignore_species=P.ignore_species,
+        snp_set=P.snp_set,
+        samples_set=P.samples_set,
+        other_samples_set=P.other_samples_set,
+        subjects_gen_f=P.subjects_gen_f,
+        max_jobs=P.max_jobs,
+        jobname=P.jobname)
+
+    # any additional parameter needs to be specifically checked that it gets to its target function
