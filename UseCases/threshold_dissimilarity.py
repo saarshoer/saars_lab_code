dbbcf720e0b38ec477db696861c1fc7f2d8902e2
saarsh <iaxq2kkh>
2022-05-29 18:10:00 +0300

    added figures

--- UseCases/threshold_dissimilarity.py
+++ UseCases/threshold_dissimilarity.py
@@ -1,6 +1,10 @@
 import os
+import numpy as np
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 from LabUtils.Utils import write_members
+from LabUtils.Utils import concat_hdf_dfs
 from LabData import config_global as config
 from LabUtils.addloglevels import sethandlers
 from LabData.DataLoaders.SubjectLoader import SubjectLoader
@@ -89,21 +93,97 @@ class P:
 #                 bad_cols.append(col)
 #     print(bad_cols)
 
+
+# if __name__ == '__main__':
+#
+#     sethandlers(file_dir=config.log_dir)
+#
+#     m = MBSNPPairwiseDistances(P)
+#     write_members(os.path.join(m._work_dir, 'PARAMS.txt'), P)
+#     m.run(
+#         species_blocks=P.species_blocks,
+#         species_set=P.species_set,
+#         ignore_species=P.ignore_species,
+#         snp_set=P.snp_set,
+#         samples_set=P.samples_set,
+#         other_samples_set=P.other_samples_set,
+#         subjects_gen_f=P.subjects_gen_f,
+#         max_jobs=P.max_jobs,
+#         jobname=P.jobname)
+#
+#     # any additional parameter needs to be specifically checked that it gets to its target function
+
+
 if __name__ == '__main__':
 
-    sethandlers(file_dir=config.log_dir)
+    base_path = '/net/mraid08/export/jafar/Microbiome/Analyses/saar/dissimilarities/'
+    diss_path = '/net/mraid08/export/genie/LabData/Analyses/saarsh/threshold_diss/mb_dists.h5'
 
-    m = MBSNPPairwiseDistances(P)
-    write_members(os.path.join(m._work_dir, 'PARAMS.txt'), P)
-    m.run(
-        species_blocks=P.species_blocks,
-        species_set=P.species_set,
-        ignore_species=P.ignore_species,
-        snp_set=P.snp_set,
-        samples_set=P.samples_set,
-        other_samples_set=P.other_samples_set,
-        subjects_gen_f=P.subjects_gen_f,
-        max_jobs=P.max_jobs,
-        jobname=P.jobname)
+    meta = pd.read_pickle(samples_path)
 
-    # any additional parameter needs to be specifically checked that it gets to its target function
+    df = concat_hdf_dfs(diss_path, axis='rows', new_index_name='Species')
+    df.index = df.index.set_levels(df.index.levels[0].str[1:], level=0)
+    df = df.join(meta['RegistrationCode'], on='SampleName1').join(meta['RegistrationCode'], on='SampleName2', lsuffix=1, rsuffix=2)
+    df['same_sample'] = df['RegistrationCode1'] == df['RegistrationCode2']
+
+    #
+    plt.figure()
+    sns.histplot(data=df, x='dissimilarity', hue='same_sample')
+    plt.yscale('log')
+
+    plt.savefig(os.path.join(base_path, 'dissimilarity histogram'))
+
+    #
+    n = 20000
+    plt.figure(figsize=(15, 10))
+    ax = sns.scatterplot(x='shared_pos', y='dissimilarity', hue='RegistrationCode1', cmap='tab20',
+                         data=df[df['same_sample']])
+    # ax = sns.jointplot(x='shared_pos', y='dissimilarity', data=df[df['same_sample']])
+    plt.axvline(n, color='red')
+    plt.axhline(1 / n, color='red')
+    plt.xscale('log')
+    plt.legend([])
+    plt.title('Same sample')
+
+    text = df[(df['same_sample']) & (df['dissimilarity'] > 0.005)]
+    for d, s, r in text[['dissimilarity', 'shared_pos', 'RegistrationCode1']].values:
+        plt.text(x=s, y=d, s=r)
+
+    for p, c in [['659088', 'pink'], ['357003', 'orange']]:
+        text = df[(df['same_sample']) & (df['dissimilarity'] > 10 ** -10) & (df['RegistrationCode1'] == p)]
+        for d, s, r in text[['dissimilarity', 'shared_pos', 'RegistrationCode1']].values:
+            plt.text(x=s, y=d, s=r, color=c)
+
+    plt.savefig(os.path.join(base_path, 'same sample'))
+
+    # plt.xlim([n, ax.get_xlim()[1]])
+    # plt.ylim([1 / n, ax.get_ylim()[1]])
+    #
+    # plt.savefig(os.path.join(base_path, 'same sample - zoom'))
+
+    #
+    print('shared_pos', '%over_detection_threshold', '%over_absolute_zero', '%dropped_comparisons')
+    for n in np.arange(5000, 40000, 5000):
+        z = df[(df['same_sample']) & (df['shared_pos'] > n) & ~(df['RegistrationCode1'].isin(['659088', '357003']))]
+        print(n,
+              "{:.2f}".format(100 * z[z['dissimilarity'] > 1 / n].shape[0] / z.shape[0]),
+              "{:.2f}".format(100 * z[z['dissimilarity'] > 0].shape[0] / z.shape[0]),
+              "{:.2f}".format(100 * (1 - z.shape[0] / df['same_sample'].sum())))
+
+    #
+    plt.figure()
+
+    species_per_person = df[df['same_sample']].reset_index().groupby('RegistrationCode1').apply(
+        lambda p: p['Species'].unique().shape[0]).sort_values()
+    species_per_person.hist()
+
+    plt.title('Species per person (both samples combined)')
+    plt.xlabel('number of species')
+    plt.ylabel('count')
+
+    plt.savefig(os.path.join(base_path, 'species per person'))
+
+    #
+    print(species_per_person.loc['659088'])
+    print(species_per_person.loc['357003'])
+
