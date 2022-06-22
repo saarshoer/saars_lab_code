4d6922d15bb86a23931c8fa7ee518ca8f2916575
saarsh <iaxq2kkh>
2022-06-22 08:55:00 +0300

    10k adjusted

--- UseCases/pnp3/pnp3_pipeline.py
+++ UseCases/pnp3/pnp3_pipeline.py
@@ -11,8 +11,9 @@ from LabMBPipeline.mmmbp import get_args, main
 import logging
 
 # touch
+study_ids = ['10K']
 body_site = 'Gut'
-run_name = 'Hanson'
+run_name = 'NebNext_new'
 MAX_JOBS = 250  # this is jobs, not threads
 
 
@@ -22,19 +23,22 @@ loader = GutMBLoader if body_site == 'Gut' else OralMBLoader
 post = '{}/DFOut/PostUniteMetadata.df' if body_site == 'Gut' else 'Oral/{}/DFOut/PostUniteMetadata.df_no_URB'
 post = os.path.join(config.mbpipeline_data_base, post.format(run_name))
 
-read_len = {'Hanson': 75, 'NovaSeq': 100,  # Gut
+read_len = {'NebNext': 100, 'NebNext_new': 100,  # Gut 10K
+            'Hanson': 75, 'NovaSeq': 100,  # Gut
             'Nextseq': 75, 'Novaseq': 100}  # Oral
 read_len = read_len[run_name]
 
-urb_num_mapped_to_subsample = {'Hanson': 8000000, 'NovaSeq': 5000000,  # Gut
+urb_num_mapped_to_subsample = {'NebNext': 5000000, 'NebNext_new': 5000000,  # Gut 10K
+                               'Hanson': 8000000, 'NovaSeq': 5000000,  # Gut
                                'Nextseq': 4000000, 'Novaseq': 2500000}  # Oral
 urb_num_mapped_to_subsample = urb_num_mapped_to_subsample[run_name]
 
-urb_min_mapped_to_retain = {'Hanson': 1000000, 'NovaSeq': 1000000,  # Gut
+urb_min_mapped_to_retain = {'NebNext': 1000000, 'NebNext_new': 1000000,  # Gut
+                            'Hanson': 1000000, 'NovaSeq': 1000000,  # Gut
                             'Nextseq': 500000, 'Novaseq': 500000}  # Oral
 urb_min_mapped_to_retain = urb_min_mapped_to_retain[run_name]
 
-base_dir = os.path.join('/net/mraid08/export/jafar/Microbiome/Analyses/saar/PNP3/pipeline/', body_site, run_name)
+base_dir = os.path.join('/net/mraid08/export/genie/LabData/Data/MBPipeline/PNP3_rerun_segata/', body_site, run_name)
 
 
 # prep
@@ -44,8 +48,13 @@ mkdirifnotexists(os.path.join(base_dir, 'tmp2', 'jobs'))
 mkdirifnotexists(os.path.join(base_dir, 'DFOut'))
 
 df = pd.read_pickle(post)
-samples = loader().get_data('segata_species', study_ids=['PNP3']).df.index
-if body_site == 'Gut':
+if study_ids == ['10K']:
+    samples = loader().get_data('segata_species', study_ids=study_ids).df_metadata.groupby('RegistrationCode').filter(
+        lambda p: (len(p) == 2) & (p['research_stage'].unique().tolist() == ['baseline', '02_00_visit'])).index
+else:
+    samples = loader().get_data('segata_species', study_ids=study_ids).df.index
+
+if body_site == 'Gut' and study_ids == ['PNP3']:
     oral_samples = OralMBLoader().get_data('segata_species', study_ids=['PNP3']).df.index
     samples = set(samples) - set(oral_samples)
 df = df.loc[df.index.isin(samples), ['URA' not in col and 'URB' not in col and 'SNP' not in col and 'SNPB' not in col for col in df.columns]]
