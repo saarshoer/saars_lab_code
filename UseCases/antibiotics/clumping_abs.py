12d904a73bab4fbdda3b3ed2247d794472672a2b
saarsh <iaxq2kkh>
2022-11-03 18:06:33 +0200

    .

--- UseCases/antibiotics/clumping_abs.py
+++ UseCases/antibiotics/clumping_abs.py
@@ -6,9 +6,9 @@ from LabQueue.qp import qp, fakeqp
 from LabUtils.addloglevels import sethandlers
 
 pval_col = 'Pval'
-# method = 'pearson'
-corr_cutoff = 0.75
-min_periods = 250
+method = 'pearson'
+min_samples = 250
+abs_corr_cutoff = 0.75
 
 study = '10K'
 run_type = 'within'
@@ -16,42 +16,37 @@ run_type = 'within'
 base_path = f'/net/mraid08/export/genie/LabData/Analyses/saarsh/anti_mwas/{study}/{run_type}'
 sig_file = os.path.join(base_path, 'mb_gwas_significant.h5')
 data_file = os.path.join(base_path, 'raw_data', 'mb_gwas_{X}_{Y}.h5')
-clump_file = os.path.join(base_path, 'clumping', 'mb_gwas_{X}_{Y}.pkl')
+clump_file = os.path.join(base_path, 'abs_clumping', 'mb_gwas_{X}_{Y}.pkl')
 
 
 def clump(X, Y):
 
-    def long_name(snp):
-        return Y, X, snp[0], snp[1]
-
     sig_df = pd.read_hdf(sig_file)
-    # sig_df = pd.concat([sig_df.head(5), sig_df.tail(5)])
-    sig_df = sig_df.xs(Y, level='Y').xs(X, level='Species')
+    sig_df = sig_df[(sig_df.index.get_level_values('Species') == X) & (sig_df.index.get_level_values('Y') == Y)]
+    # sig_df = pd.concat([sig_df.head(50), sig_df.tail(5)])
     sig_df = sig_df.sort_values(pval_col, ascending=True)
 
-    data_df = pd.read_hdf(data_file.replace('{X}', X).replace('{Y}', Y))
-    data_df = data_df.xs(Y, level='Y').xs(X, level='Species')
+    data = pd.read_hdf(data_file.replace('{X}', X).replace('{Y}', Y))
+    data = data.reset_index(sig_df.index.names)
+    data['Position'] = data['Position'].astype(int)
+    data = data.groupby(sig_df.index.names)[[data.columns[-1]]]
 
-    sig_snp = None
     sig_snps = []
     all_snps = {}
     for new_snp in sig_df.index:
-        new_snp_data = data_df.xs(new_snp[0], level='Contig').xs(str(new_snp[1]), level='Position')
         add = True
         for sig_snp in sig_snps:
-            sig_snp_data = data_df.xs(sig_snp[0], level='Contig').xs(str(sig_snp[1]), level='Position')
-            # corr = new_snp_data.corr(sig_snp_data, method=method, min_periods=min_periods)  # too slow
-            shared_samples = new_snp_data.index.intersection(sig_snp_data.index)
-            if len(shared_samples) >= min_periods:
-                corr, pval = pearsonr(new_snp_data.loc[shared_samples], sig_snp_data.loc[shared_samples])
-            else:
-                corr, pval = 0, 1
-            if corr >= corr_cutoff:
-                add = False
-                break
+            common_samples = set(data.get_group(new_snp).index) & set(data.get_group(sig_snp).index)
+            if len(common_samples) >= min_samples:
+                corr = data.get_group(new_snp).corrwith(data.get_group(sig_snp), method=method)[0]
+                if abs(corr) >= abs_corr_cutoff:
+                    add = False
+                    break
         if add:
             sig_snps.append(new_snp)
-        all_snps[long_name(new_snp)] = long_name(new_snp) if add else long_name(sig_snp)
+            all_snps[new_snp] = new_snp
+        else:
+            all_snps[new_snp] = sig_snp
 
     results_file = clump_file.replace('{X}', X).replace('{Y}', Y)
     with open(results_file, 'wb') as f:
@@ -69,12 +64,12 @@ if __name__ == '__main__':
 
     os.makedirs(os.path.dirname(clump_file))#, exist_ok=True)
 
-    with qp(jobname='clumping', _tryrerun=True, _specific_nodes='plink') as q:
+    with qp(jobname='abs_clumping', _tryrerun=True, _specific_nodes='plink') as q:
         q.startpermanentrun()
         tkttores = {}
 
         significant_df = pd.read_hdf(sig_file)
-        # significant_df = pd.concat([significant_df.head(5), significant_df.tail(5)])
+        # significant_df = pd.concat([significant_df.head(50), significant_df.tail(5)])
 
         print('start sending jobs')
         runs = significant_df.reset_index()[['Species', 'Y']].drop_duplicates().reset_index(drop=True)
@@ -96,5 +91,5 @@ if __name__ == '__main__':
                 run_results = pickle.load(f)
             significant_df.loc[run_results.keys()] = significant_df.loc[run_results.keys()].assign(
                 clumping=run_results.values())
-        significant_df.to_hdf(sig_file.replace('.h5', '_clumping.h5'), key='snps')
+        significant_df.to_hdf(sig_file.replace('.h5', '_abs_clumping.h5'), key='snps')
         print('finished df update')
