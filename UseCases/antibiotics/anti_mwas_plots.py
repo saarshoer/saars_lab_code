701e6fe65ee696bd1d2186eb97d819034d41bbdc
saarsh <iaxq2kkh>
2022-11-10 14:42:39 +0200

    .

--- UseCases/antibiotics/anti_mwas_plots.py
+++ UseCases/antibiotics/anti_mwas_plots.py
@@ -33,18 +33,23 @@ def color_by_coef(sp_df: pd.DataFrame, **kwargs):
     else:
         d['linewidths'] = 1
 
-    if 'clumping' in sp_df.columns:
+    d['facecolor'] = (sp_df['Coef'] > 0).map(color_dict)
+    if 'feature_importance' in sp_df.columns:
+        is_important = sp_df['feature_importance'] > 0
+        d['facecolor'].loc[is_important] = [(c[0], c[1], c[2], 0.8) for c in d['facecolor'].loc[is_important]]
+        d['facecolor'].loc[~is_important] = [(c[0], c[1], c[2], 0.2) for c in d['facecolor'].loc[~is_important]]
+        d['alpha'] = None
+    elif 'clumping' in sp_df.columns:
         is_clumping_rep = sp_df.index == sp_df['clumping']
-        d['facecolor'] = (sp_df['Coef'] > 0).map(color_dict)
         d['facecolor'].loc[is_clumping_rep] = [(c[0], c[1], c[2], 0.8) for c in d['facecolor'].loc[is_clumping_rep]]
         d['facecolor'].loc[~is_clumping_rep] = [(c[0], c[1], c[2], 0.2) for c in d['facecolor'].loc[~is_clumping_rep]]
-        d['facecolor'] = d['facecolor'].values
         d['alpha'] = None
         if 'text' in sp_df.columns:
             sp_df.loc[~is_clumping_rep, 'text'] = None
     else:
-        d['facecolor'] = (sp_df['Coef'] > 0).map(color_dict).values
         d['alpha'] = 0.5
+    d['facecolor'] = d['facecolor'].values
+
     if 'text' in sp_df.columns:
         d['text'] = sp_df['text']
 
@@ -64,15 +69,16 @@ def color_by_coef(sp_df: pd.DataFrame, **kwargs):
 
 def text_func(df: pd.DataFrame, **kwargs):
     df = df.reset_index().drop_duplicates(list(df.index.names))
+    n_sig = f"{sum(df[kwargs['pval_col']] <= kwargs['pval_cutoff']):,}"
 
     text = f"N = between {df['N'].min():,} and {df['N'].max():,} samples per SNP\n" + \
-           f"{sum(df[kwargs['pval_col']] <= kwargs['pval_cutoff']):,}/{df.shape[0]:,} " + \
-           f"SNPs passed {kwargs['pval_cutoff']:.2e} p-value cutoff"
+           f"{n_sig}/{df.shape[0]:,} SNPs passed {kwargs['pval_cutoff']:.2e} p-value cutoff"
     if 'validation_level' in df.columns:
-        text = text + f"\n{df[df['validation_level'] == 'CI overlap'].shape[0]:,}/" + \
-                        f"{df[df['validation_level'] != 'not tested'].shape[0]:,} " + \
+        text = text + f"\n{df[df['validation_level'] == 'CI overlap'].shape[0]:,}/{n_sig} " + \
                         f"SNPs CI overlaps with validation cohort"
-    if 'clumping' in df.columns:
+    if 'feature_importance' in df.columns:
+        text = text + f"\n{(df['feature_importance'].fillna(0) != 0).sum():,}/{n_sig} effective SNPs"
+    elif 'clumping' in df.columns:
         text = text + f"\n{df['clumping'].unique().shape[0]:,} clumped SNPs"
 
     if (df['Y'] != df['Species']).any():
@@ -85,6 +91,7 @@ if __name__ == '__main__':
 
     study = '10K'
     run_type = 'within'
+    data_plots = False
 
     input_dir = f'/net/mraid08/export/genie/LabData/Analyses/saarsh/anti_mwas/{study}/{run_type}'
     output_dir = os.path.join(input_dir, 'figs', 'species')
@@ -92,16 +99,25 @@ if __name__ == '__main__':
 
     ydata_fname = os.path.join(os.path.dirname(input_dir), 'data_frames', 'abundance.df')
 
-    mwas_df = pd.read_hdf(os.path.join(input_dir, 'mb_gwas_significant_full.h5'))[['clumping', 'validation_level']]
-    annotations_df = pd.read_hdf(os.path.join(input_dir, 'snps_gene_annotations_short.h5'))[['text']]
-    annotations_df = mwas_df.join(annotations_df)
-    del mwas_df
+    mwas_df = pd.read_hdf(os.path.join(input_dir, 'mb_gwas_significant_validation_models.h5'))[['Pval', 'feature_importance', 'validation_level']]
+    # mwas_df = mwas_df[mwas_df.index.get_level_values('Y') == 'Rep_959']
+    annotations_df = pd.read_hdf(os.path.join(input_dir, 'snps_gene_annotations_short.h5'))[['GeneID', 'text']]
+    mwas_df = mwas_df.join(annotations_df)
+    del annotations_df
+
+    if not data_plots:
+        group_text = pd.DataFrame(mwas_df.sort_values('Pval').groupby(['Y', 'Species', 'GeneID']).apply(
+            lambda g: g.reset_index()[mwas_df.index.names].iloc[0].tolist() + [
+                f'{g["text"].fillna("").iloc[0]}'+(f'({g.shape[0]})' if g.shape[0] > 1 else '')]).tolist(),
+                    columns=list(mwas_df.index.names) + ['text']).set_index(mwas_df.index.names)
+        mwas_df = mwas_df.drop('text', axis=1).join(group_text)
+    mwas_df = mwas_df.drop(['Pval', 'GeneID'], axis=1)
 
     counts = pd.read_hdf(os.path.join(input_dir, 'mb_gwas_counts.h5'))
     alpha = 0.01/counts.sum().iloc[0]
     del counts
 
-    fmt = lambda coef, pos: f'$2^{{{coef:.1f}}}$'
+    fmt1d = lambda coef, pos: f'$2^{{{coef:.1f}}}$'
 
     # queue
     os.chdir(jobs_dir)
@@ -112,13 +128,15 @@ if __name__ == '__main__':
         tkttores = {}
 
         print('start sending jobs')
-        for mwas_fname in glob.glob(os.path.join(input_dir, 'raw_hdfs', 'mb_gwas_Rep_*_Rep_*.h5')):
+        for mwas_fname in glob.glob(os.path.join(input_dir, 'raw_hdfs', 'mb_gwas_Rep_959_Rep_959.h5')):
             data_fname = mwas_fname.replace('raw_hdfs', 'raw_data')
-            kwargs = {'mwas_fname': mwas_fname,
-                      'data_fname': data_fname if os.path.exists(data_fname) else None,
+            if data_plots & ~os.path.exists(data_fname):
+                continue
+            kwargs = {'mwas_fname': mwas_fname if not data_plots else None,
+                      'data_fname': data_fname if data_plots else None,
                       'ydata_fname': ydata_fname,
                       'out_dir': output_dir,
-                      'annotations_df': annotations_df,
+                      'annotations_df': mwas_df,
                       'manhattan_draw_func': color_by_coef,
                       'manhattan_text_func': text_func,
                       'r2_col': 'R2',
@@ -126,7 +144,7 @@ if __name__ == '__main__':
                       'coef_col': 'Coef',
                       'pval_col': 'Pval',
                       'pval_cutoff': alpha,
-                      'fmt': fmt}
+                      'fmt': fmt1d}
             tkttores[mwas_fname] = q.method(mwas_plots.run, kwargs=kwargs)
         print('finished sending jobs')
 
