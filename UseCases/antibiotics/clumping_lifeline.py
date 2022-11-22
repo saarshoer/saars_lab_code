438fb1b3299ff9d9676c55a38c45bb8a2804f8fb
saarsh <iaxq2kkh>
2022-11-22 10:01:43 +0200

    .

--- UseCases/antibiotics/clumping_lifeline.py
+++ UseCases/antibiotics/clumping_lifeline.py
@@ -1,4 +1,5 @@
 import os
+import glob
 import pickle
 import pandas as pd
 from scipy.stats import pearsonr
@@ -63,9 +64,9 @@ if __name__ == '__main__':
     os.chdir(jobs_dir)
     sethandlers(file_dir=jobs_dir)
 
-    # os.makedirs(os.path.dirname(clump_file))#, exist_ok=True)
+    os.makedirs(os.path.dirname(clump_file))#, exist_ok=True)
 
-    with qp(jobname='BLclumping', _tryrerun=True, _mem_def='20G') as q:
+    with qp(jobname='BLclumping', _tryrerun=True, _mem_def='5G') as q:
         q.startpermanentrun()
         tkttores = {}
 
@@ -75,8 +76,8 @@ if __name__ == '__main__':
         print('start sending jobs')
         runs = significant_df.reset_index()[['Species', 'Y']].drop_duplicates().reset_index(drop=True)
         for i, (speciesX, speciesY) in runs.iterrows():
-            if not os.path.exists(clump_file.replace('{X}', speciesX).replace('{Y}', speciesY)):
-                tkttores[i] = q.method(clump, (speciesX, speciesY))
+            # if not os.path.exists(clump_file.replace('{X}', speciesX).replace('{Y}', speciesY)):
+            tkttores[i] = q.method(clump, (speciesX, speciesY))
         print('finished sending jobs')
 
         print('start waiting for jobs')
@@ -84,14 +85,14 @@ if __name__ == '__main__':
         for k, v in tkttores.items():
             results_files.append(q.waitforresult(v))
         print('finished waiting for jobs')
-        # results_files = glob.glob(clump_file.replace('{X}', '*').replace('{Y}', '*'))
-        #
-        # print('start df update')
-        # significant_df['clumping'] = None
-        # for results_file in results_files:
-        #     with open(results_file, 'rb') as f:
-        #         run_results = pickle.load(f)
-        #     significant_df.loc[run_results.keys()] = significant_df.loc[run_results.keys()].assign(
-        #         clumping=run_results.values())
-        # significant_df.to_hdf(sig_file.replace('.h5', '_abs_clumping.h5'), key='snps')
-        # print('finished df update')
+        results_files = glob.glob(clump_file.replace('{X}', '*').replace('{Y}', '*'))
+
+        print('start df update')
+        significant_df['clumping'] = None
+        for results_file in results_files:
+            with open(results_file, 'rb') as f:
+                run_results = pickle.load(f)
+            significant_df.loc[run_results.keys()] = significant_df.loc[run_results.keys()].assign(
+                clumping=run_results.values())
+        significant_df.to_hdf(sig_file.replace('.h5', '_abs_clumping.h5'), key='snps')
+        print('finished df update')
--- UseCases/antibiotics/models.py
+++ UseCases/antibiotics/models.py
@@ -23,12 +23,6 @@ def create_model(X, y, fname=None):
 
 
 def score_models(sy, sx):
-    # data = pd.read_hdf(os.path.join(run_dir, 'raw_data', f'mb_gwas_{sx if run_type == "within" else "Rep_all"}_{sy}.h5'))
-    # data = data.reset_index().drop_duplicates().set_index(data.index.names).iloc[:, -1]
-    # data.to_hdf(os.path.join(run_dir, 'raw_data', f'mb_gwas_{sx if run_type == "within" else "Rep_all"}_{sy}_new.h5'), key='snps', complevel=9)
-    # del data
-    # return
-
     xdata = pd.read_hdf(os.path.join(run_dir, 'raw_data', f'mb_gwas_{sx if run_type == "within" else "Rep_all"}_{sy}.h5')).to_frame('MAF')
     xdata = xdata[xdata.index.get_level_values('Species') == sx]  # makes it much more memory efficient
     xdata['Position'] = xdata.index.get_level_values('Position').astype(int)
@@ -61,7 +55,7 @@ if __name__ == "__main__":
     os.chdir(jobs_dir)
     sethandlers(file_dir=jobs_dir)
 
-    os.makedirs(os.path.join(run_dir, 'models'))
+    # os.makedirs(os.path.join(run_dir, 'models'))
 
     with qp(jobname='Kmodels', _tryrerun=True, _mem_def='10G') as q:
         q.startpermanentrun()
@@ -71,14 +65,10 @@ if __name__ == "__main__":
         # sig_df = sig_df[sig_df.index.get_level_values('Species') == 'Rep_595']
         models = pd.DataFrame(columns=['Y', 'Species', 'base_score', 'snps_score'])
 
-        # all_probs = []
         print('start sending jobs')
         for i, (speciesY, speciesX) in sig_df.reset_index()[['Y', 'Species']].drop_duplicates().iterrows():
-            # if not os.path.exists(os.path.join(run_dir, 'models', f'{speciesX}_{speciesY}.pkl')):
-            #     print(speciesX, speciesY)
-                # if speciesY not in all_probs:
-                #     all_probs.append(speciesY)
-            tkttores[i] = q.method(score_models, (speciesY, speciesX))
+            if not os.path.exists(os.path.join(run_dir, 'models', f'{speciesX}_{speciesY}.pkl')):
+                tkttores[i] = q.method(score_models, (speciesY, speciesX))
         print('finished sending jobs')
 
         print('start waiting for jobs')
