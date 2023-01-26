aa1215cfeddca4588130d3d26f78890c22b80cd0
saarsh <iaxq2kkh>
2023-01-26 10:19:48 +0200

    accommodate big files

--- UseCases/antibiotics/common_snps.py
+++ UseCases/antibiotics/common_snps.py
@@ -3,7 +3,7 @@ import glob
 import numpy as np
 import pandas as pd
 
-run_type = 'within'
+run_type = 'between'
 coef_col = 'Coef'
 
 base_path = '/net/mraid08/export/jafar/Microbiome/Analyses/saar/antibiotics'
@@ -19,7 +19,8 @@ files2 = [os.path.basename(f) for f in glob.glob(os.path.join(dir2, '*'))]
 common_files = set(files1) & set(files2)
 
 dfs = []
-for file in common_files:
+part = 100
+for i, file in enumerate(common_files):
     print(file)
     df1 = pd.read_hdf(os.path.join(dir1, file))[['N', coef_col]]
     df2 = pd.read_hdf(os.path.join(dir2, file))[['N', coef_col]]
@@ -27,10 +28,9 @@ for file in common_files:
     df = df1.join(df2, how='inner', lsuffix=f'_{study1}', rsuffix=f'_{study2}')
     dfs.append(df)
 
+    if (run_type == 'between') & (i % part == 0) & (i != 0):
+        pd.concat(dfs).to_hdf(os.path.join(base_path, f'common_snps_{run_type}_pt{int(i/part+1)}.h5'), key='snps', complevel=9)
+        dfs = []
+
 if run_type == 'within':
     pd.concat(dfs).to_hdf(os.path.join(base_path, f'common_snps_{run_type}.h5'), key='snps', complevel=9)
-else:
-    n_parts = 4
-    for i in np.arange(n_parts):
-        pd.concat(dfs[int(len(dfs)*i/n_parts):int(len(dfs)*(i+1)/n_parts)]).to_hdf(
-            os.path.join(base_path, f'common_snps_{run_type}_pt{i+1}.h5'), key='snps', complevel=9)
